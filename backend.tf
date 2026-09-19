11919bca5dfba41f80a03400bb35c33be711d900
nihal <nihal@gmail.com>
2026-09-19 21:41:01 +0530

    1

--- backend.tf
+++ backend.tf
@@ -1,10 +1,9 @@
 terraform {
-  backend s3 {
-    bucket         = "my-faizan-terraform-state-bucket"
-    key            = "env/dev/terraform.tfstate"
-    region         = "ap-south-1"
-    encrypt        = true
+  backend "s3" {
+    bucket       = "my-faizan-terraform-state-bucket"
+    key          = "env/dev/terraform.tfstate"
+    region       = "ap-south-1"
+    encrypt      = true
     use_lockfile = true
   }
-
 }
